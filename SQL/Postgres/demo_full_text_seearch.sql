999420698042c9c7fc193afab230f13c813a9bc9
Dmitriy "DK" Korobskiy <dk@nete.com>
2019-11-17 18:10:01 -0500

    1

--- SQL/Postgres/demo_full_text_seearch.sql
+++ SQL/Postgres/demo_full_text_seearch.sql
@@ -1,3 +1,50 @@
+-- region ts_rank()
+SELECT
+  ts_rank(to_tsvector('english', 'one two three'),
+          plainto_tsquery('english', 'one two three'));
+-- 0.268
+
+SELECT
+  ts_rank(to_tsvector('english', 'one two three four five six seven eight nine ten'),
+          plainto_tsquery('english', 'one two three four five six seven eight nine ten'));
+-- 0.987
+
+SELECT
+  ts_rank(to_tsvector('english', 'one two three four five six seven eight nine ten eleven'),
+          plainto_tsquery('english', 'one two three four five six seven eight nine ten eleven'));
+-- 0.994
+
+SELECT
+  ts_rank(to_tsvector('english', 'one two three four five six seven eight nine ten eleven twelve'),
+          plainto_tsquery('english', 'one two three four five six seven eight nine ten eleven'));
+-- 0.994
+
+SELECT
+  ts_rank(to_tsvector('english', 'one two three four five six seven eight nine ten eleven'),
+          plainto_tsquery('english', 'one two three four five six seven eight nine ten eleven twelve'));
+-- 0.994
+
+SELECT
+  ts_rank(to_tsvector('english', 'one two three four five six seven eight nine ten eleven twelve'),
+          plainto_tsquery('english', 'one two three four five six seven eight nine ten eleven twelve'));
+-- 0.997
+
+SELECT
+  ts_rank(to_tsvector('english', 'one two three four five six seven eight nine ten eleven twelve thirteen'),
+          plainto_tsquery('english', 'one two three four five six seven eight nine ten eleven twelve thirteen'));
+-- 0.999
+
+SELECT
+  ts_rank(to_tsvector('english', 'one two three'),
+          plainto_tsquery('english', 'one two three'), 32);
+-- 0.21
+
+SELECT
+  ts_rank(to_tsvector('english', 'one two three'),
+          plainto_tsquery('english', 'one two three'), 8);
+-- 0.09
+-- endregion
+
 -- 1:1 query conversion
 SELECT 'Under & pressure'::TSQUERY;
 
@@ -13,9 +60,10 @@ SELECT plainto_tsquery('english', 'Something was under some sort of pressure.');
 SELECT websearch_to_tsquery('english', 'Something was under some sort of pressure.');
 
 -- Produce OR-tsquery
-SELECT array_to_string(tsvector_to_array(to_tsvector('english', 'Something was under some sort of pressure.')), ' | ');
+SELECT
+  array_to_string(tsvector_to_array(to_tsvector('english', 'Something was under some sort of pressure.')), ' | ');
 
-SELECT title, ts_rank(to_tsvector('english', title), plainto_tsquery('english', 'tsquery tsvector')) as rank
+SELECT title, ts_rank(to_tsvector('english', title), plainto_tsquery('english', 'tsquery tsvector')) AS rank
   FROM
     (
       VALUES
@@ -27,7 +75,7 @@ variants of the same word.'),
 a tsquery (query).'),
         ('The @@ operator also supports text input, allowing explicit conversion of a text string to tsvector or tsquery to be skipped in simple cases.')
     ) AS t(title)
-WHERE to_tsvector('english', title) @@ plainto_tsquery('english', 'tsquery tsvector');
+ WHERE to_tsvector('english', title) @@ plainto_tsquery('english', 'tsquery tsvector');
 
 /*
 The tsvector type represents a document in a form optimized for text search; the tsquery type similarly represents a
