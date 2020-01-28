fffda433845803d244320dbf30d020da816c41ce
DKroot <dk@nete.com>
2020-01-28 18:15:49 -0500

    Finalize Vue.js-2.5 scratchpad project

--- Client_Side/Vue.js-2.5/app-pure-vue.js
+++ Client_Side/Vue.js-2.5/app-pure-vue.js
@@ -1,39 +1,38 @@
-define(["require", "exports", "vue", "vue-router", "./messageComponent", "reflect-metadata"], function (require, exports, vue_1, vue_router_1, messageComponent_1) {
+define(["require", "exports", "vue", "vue-router", "./messageComponent-pure-vue"], function (require, exports, vue_1, vue_router_1, messageComponent_pure_vue_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    // import ParameterizedComponent from "./parameterizedComponent";
     //endregion
     vue_1.default.use(vue_router_1.default);
     const routes = [
         {
             path: "/",
-            component: messageComponent_1.MessageComponent
-        } /*,
-        {
-          path: "/cwp/:id",
-          component: ParameterizedComponent
-        }*/
+            component: messageComponent_pure_vue_1.default
+        }
     ];
     const router = new vue_router_1.default({
         routes
     });
-    // noinspection JSUnusedLocalSymbols
     const appOptions = {
         router,
-        data: {
-            user: null,
-            loading: false
+        data() {
+            return {
+                user: null,
+                loading: false
+            };
         },
         methods: {
             onLoading() {
+                // @ts-ignore
                 this.loading = true;
                 console.log("Loading...");
             },
             onLoaded() {
+                // @ts-ignore
                 this.loading = false;
                 console.log("Loaded.");
             },
             onLoadingError(error, serviceUrl) {
+                // @ts-ignore
                 this.loading = false;
                 console.log("Loading error for", serviceUrl, error.response);
             }
