5772a23ae80d21e266a08c6650bce445b8afa49a
DKroot <dk@nete.com>
2020-01-28 18:11:52 -0500

    Update tslint and Vue to 2.4.4

---
+++ Client_Side/Vue.js-2.5/app-pure-vue.ts
@@ -0,0 +1,51 @@
+import * as Vue from "vue";
+import * as VueRouter from "vue-router";
+import { AxiosError } from "axios";
+
+//region App components
+import MessageComponent from "./messageComponent-pure-vue";
+//endregion
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    component: MessageComponent
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+const appOptions = {
+  router,
+  data() {
+    return {
+      user: null,
+      loading: false
+    };
+  },
+  methods: {
+    onLoading() {
+      // @ts-ignore
+      this.loading = true;
+      console.log("Loading...");
+    },
+
+    onLoaded() {
+      // @ts-ignore
+      this.loading = false;
+      console.log("Loaded.");
+    },
+
+    onLoadingError(error: AxiosError, serviceUrl: string) {
+      // @ts-ignore
+      this.loading = false;
+      console.log("Loading error for", serviceUrl, error.response);
+    }
+  }
+};
+
+new Vue(appOptions).$mount("#app");
