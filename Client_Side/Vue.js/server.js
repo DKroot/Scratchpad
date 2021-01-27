c614401f138aa4ba5b4752215e7feb5453d30487
Dmitriy "DK" Korobskiy <dkroot2@gmail.com>
2021-01-26 20:03:26 -0500

    DK

--- Client_Side/Vue.js/server.js
+++
@@ -1,6 +0,0 @@
-const http = require("http");
-const nStatic = require("node-static");
-const PORT = 4292;
-
-const fileServer = new nStatic.Server('./');
-http.createServer((req, res) => fileServer.serve(req, res)).listen(PORT);
