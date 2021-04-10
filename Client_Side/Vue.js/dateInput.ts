492c709248368ea669d38d6de4945e180ce6faa0
Dmitriy "DK" Korobskiy <dkroot2@gmail.com>
2021-04-10 17:03:44 -0400

    DK

--- Client_Side/Vue.js/dateInput.ts
+++ Client_Side/Vue.js/dateInput.ts
@@ -12,12 +12,17 @@ import * as DatePick from "vue-date-pick";
  */
 @Component({
   /*
-   Bind `value` to the child input.
-   Propagate the `input` event with the changed value payload to the parent.
-   See http://vuejs.org/v2/guide/components.html#Using-v-model-on-Components.
+   Bind `value` to the child input and propagate the `input` event with the changed value
+   payload to the parent. See http://vuejs.org/v2/guide/components.html#Using-v-model-on-Components.
+
+   Propagate some <date-pick> properties via `v-bind` fields.
+   Propagate some <input> attributes.
   */
-  template: `<date-pick :value="value" @input="$emit('input', $event)"
-    format="MM/DD/YYYY" :start-week-on-sunday="true"></date-pick>`,
+  template: `
+    <date-pick :value="value" @input="$emit('input', $event)"
+               format="MM/DD/YYYY" :start-week-on-sunday="true"
+               v-bind="{selectableYearRange}"
+               :inputAttributes="{id, name, tabindex}"></date-pick>`,
   components: {
     'date-pick': DatePick as any
   }
@@ -25,4 +30,24 @@ import * as DatePick from "vue-date-pick";
 export default class DateInput extends Vue {
   // The property used by `v:model`
   @Prop() value: string;
+
+  //region Propagated <date-pick> props
+  /*
+  Other <date-pick> props can be propagated via `v-bind` fields or in their entirety via
+  `v-bind=$props` (but all of them would have to be explicitly declared as props in anyway).
+  */
+
+  @Prop(Object) selectableYearRange: object;
+  //endregion
+
+  //region Propagated <input> attributes
+  @Prop() id: string;
+  @Prop() name: string;
+  @Prop(Number) tabindex: number;
+  //endregion
+
+  /*
+  Other component attributes (`$attrs`) are to be avoided lest they are rendered internally in
+  <date-pick> <div>-s.
+  */
 }
