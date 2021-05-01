5187088be3cdd4c9e94a067c820c925e5066f025
Dmitriy "DK" Korobskiy <dkroot2@gmail.com>
2021-05-01 10:20:33 -0400

    DK

---
+++ Groovy/GroovyDemos/src/org/houseofsoft/groovy/demos/Dates.groovy
@@ -0,0 +1,16 @@
+package org.houseofsoft.groovy.demos
+
+import java.sql.Timestamp
+import java.time.LocalDateTime
+
+def nowLocalDateTime = LocalDateTime.now()
+println "Local time: ${nowLocalDateTime}"
+println "Formatted: ${nowLocalDateTime.format('MM/dd/yy HH:mm')}"
+
+def nowDateTime = new Date()
+println "Time: ${nowDateTime}"
+println "Formatted: ${nowDateTime.toLocalDateTime().format('MM/dd/yy HH:mm')}"
+
+Timestamp nowTimestamp = nowDateTime.toTimestamp()
+println "Timestamp: ${nowTimestamp}"
+println "Formatted: ${nowTimestamp.toLocalDateTime().format('MM/dd/yy HH:mm')}"
