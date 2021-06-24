35a84fecfbbf34c37e78c6154ef426b81396a03b
Dmitriy "DK" Korobskiy <dkroot2@gmail.com>
2021-06-24 09:04:42 -0400

    DK

--- Bash/demo-gnu-parallel-simple.sh
+++ Bash/demo-gnu-parallel-simple.sh
@@ -17,32 +17,39 @@ echo -e "\n## Running under ${USER}@${HOSTNAME} at ${PWD} ##\n"
 echo -e "## Parallel runs with # of jobs = # of CPU cores using xargs using GNU parallel ##"
 parallel_cores=$(parallel --number-of-cores)
 
+echo -e "\n### Parallelize a sequence ###"
+# shellcheck disable=SC2016
+parallel --halt soon,fail=1 --line-buffer --tagstring '|job#{#}/{= $_=total_jobs() =} s#{%}|' "set -e
+    echo 'Processing {} ...'
+    echo '{}: done.'" ::: {1..10}
+#-verbose
+
 echo -e "\n### Simple command scaled according to a number of cores ###"
 # For certain jobs there is no need to continue if one of the jobs fails and has an exit code different from 0.
 # GNU parallel will stop spawning new jobs with --halt soon,fail=1
 # With --halt now,fail=1 the running jobs will be killed immediately
-parallel --halt soon,fail=1 --verbose --line-buffer --tagstring '|job#{#} s#{%}|' \
+parallel --halt soon,fail=1 --line-buffer --tagstring '|job#{#} s#{%}|' \
               echo "Executing job {#} of {= \$_=total_jobs() =}" ::: $(seq -s ' ' "${parallel_cores}")
 
 echo -e "\n### Simple command with quoted arguments ###"
 # For certain jobs there is no need to continue if one of the jobs fails and has an exit code different from 0.
 # GNU parallel will stop spawning new jobs with --halt soon,fail=1
 # With --halt now,fail=1 the running jobs will be killed immediately
-parallel --halt soon,fail=1 --verbose --line-buffer --tagstring '|job#{#} s#{%}|' echo "Processing '{}' ..." ::: \
+parallel --halt soon,fail=1 --line-buffer --tagstring '|job#{#} s#{%}|' echo "Processing '{}' ..." ::: \
   arg1 'arg(2)' arg3
 
 echo -e "\n### Simple command piping arguments to stdout ###"
-parallel --halt soon,fail=1 --verbose --line-buffer --tagstring '|job#{#} s#{%}|' 'echo {} | cat' ::: \
+parallel --halt soon,fail=1 --line-buffer --tagstring '|job#{#} s#{%}|' 'echo {} | cat' ::: \
   arg1 'arg(2)' arg3
 
 echo -e "\n### A command with multi-line arguments ###"
-parallel --null --halt soon,fail=1 --verbose --line-buffer --tagstring '|job#{#} s#{%}|' \
+parallel --null --halt soon,fail=1 --line-buffer --tagstring '|job#{#} s#{%}|' \
   echo "Processing {} ..." ::: '1: line1
 1: line2' '2: line 1
 2: line 2'
 
 echo -e "\n### Processing multiple arguments at a time ###"
-parallel --halt soon,fail=1 --verbose --line-buffer --tagstring '|job#{#} s#{%}|' "set -e;
+parallel --halt soon,fail=1 --line-buffer --tagstring '|job#{#} s#{%}|' "set -e;
   echo 'Processing group ({}): {1} + {2} + {3}'" ::: 1 4 7 10 :::+ 2 5 8 :::+ 3 6 9
 
 echo -e "\n### A one-line script. WARNING: Failures are not processed! ###"
