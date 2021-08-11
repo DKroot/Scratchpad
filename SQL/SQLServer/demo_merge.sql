71ef77a0953b411a4e64890afa8caf3b2c49f4fd
Dmitriy "DK" Korobskiy <dkroot2@gmail.com>
2021-08-11 18:15:50 -0400

    DK

---
+++ SQL/SQLServer/demo_merge.sql
@@ -0,0 +1,29 @@
+WITH sample_data AS (
+  SELECT 1 AS group_id, 2001 AS year, 10001 AS tour_id, 'San Francisco' AS city
+  UNION ALL
+  SELECT 1 AS group_id, 2009 AS year, 9010 AS tour_id, 'Chicago' AS city
+  UNION ALL
+  SELECT 1 AS group_id, 2009 AS year, 9002 AS tour_id, 'New Orleans' AS city
+  UNION ALL
+  SELECT 2 AS group_id, 2006 AS year, 6001 AS tour_id, 'Washington' AS city
+  UNION ALL
+  SELECT 2 AS group_id, 2007 AS year, 7001 AS tour_id, 'New York' AS city
+  UNION ALL
+  SELECT 3 AS group_id, 2008 AS year, 8001 AS tour_id, 'Seattle' AS city
+)
+SELECT *
+INTO tmp_demo_merge
+FROM sample_data;
+
+-- INSERT if doesn't exist
+WITH data AS (
+  SELECT 1 AS group_id, 2001 AS year, 10001 AS tour_id, 'San Francisco' AS city
+)
+INSERT
+INTO tmp_demo_merge(group_id, year, tour_id, city)
+SELECT group_id, year, tour_id, city
+FROM data
+WHERE NOT exists(SELECT 1
+                 FROM
+                   tmp_demo_merge tdm
+                     JOIN data ON tdm.tour_id = data.tour_id);
