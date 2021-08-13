f87c5193a2e81d6566f4f9440ccc24456ec1e54c
Dmitriy "DK" Korobskiy <dkroot2@gmail.com>
2021-08-13 15:22:54 -0400

    DK

--- SQL/SQLServer/demo-try-catch.sql
+++ SQL/SQLServer/demo-try-catch.sql
@@ -3,16 +3,15 @@ BEGIN TRY
   SELECT 1 / 0;
 END TRY BEGIN CATCH
   SELECT
-    ERROR_NUMBER() AS ErrorNumber, ERROR_SEVERITY() AS ErrorSeverity, ERROR_STATE() AS ErrorState,
-    ERROR_PROCEDURE() AS ErrorProcedure, ERROR_LINE() AS ErrorLine, ERROR_MESSAGE() AS ErrorMessage;
+    error_number() AS errornumber, error_severity() AS errorseverity, error_state() AS errorstate,
+    error_procedure() AS errorprocedure, error_line() AS errorline, error_message() AS errormessage;
 END CATCH;
 GO
 
 BEGIN TRY
   -- Generate a divide-by-zero error.
   SELECT 1 / 0;
-END TRY
-BEGIN CATCH
+END TRY BEGIN CATCH
   THROW;
 END CATCH;
 GO
---
+++ SQL/SQLServer/demo_dates.sql
@@ -0,0 +1,8 @@
+-- Now
+SELECT current_timestamp;
+
+-- Now
+SELECT getdate();
+
+-- Truncate to current date
+SELECT cast(getdate() AS DATE);
