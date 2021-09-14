e6df3a4cc9833fb1277eb5d5927e6da431d2c095
Dmitriy "DK" Korobskiy <dkroot2@gmail.com>
2021-09-14 18:45:16 -0400

    DK

--- SQL/SQLServer/demo_merge.sql
+++ SQL/SQLServer/demo_merge.sql
@@ -1,47 +1,46 @@
+-- Sample data
 WITH sample_data AS (
-  SELECT 1 AS group_id, 2001 AS year, 10001 AS tour_id, 'San Francisco' AS city
+  SELECT 1 AS tour_id, 1 AS group_id, 2001 AS year, 'San Francisco' AS city
   UNION ALL
-  SELECT 1 AS group_id, 2009 AS year, 9010 AS tour_id, 'Chicago' AS city
+  SELECT 2 AS tour_id, 1 AS group_id, 2009 AS year, 'Chicago' AS city
   UNION ALL
-  SELECT 1 AS group_id, 2009 AS year, 9002 AS tour_id, 'New Orleans' AS city
+  SELECT 3 AS tour_id, 1 AS group_id, 2009 AS year, 'New Orleans' AS city
   UNION ALL
-  SELECT 2 AS group_id, 2006 AS year, 6001 AS tour_id, 'Washington' AS city
+  SELECT 4 AS tour_id, 2 AS group_id, 2006 AS year, 'Washington' AS city
   UNION ALL
-  SELECT 2 AS group_id, 2007 AS year, 7001 AS tour_id, 'New York' AS city
+  SELECT 5 AS tour_id, 2 AS group_id, 2007 AS year, 'New York' AS city
   UNION ALL
-  SELECT 3 AS group_id, 2008 AS year, 8001 AS tour_id, 'Seattle' AS city
+  SELECT 6 AS tour_id, 3 AS group_id, 2008 AS year, 'Seattle' AS city
 )
 SELECT *
-INTO tmp_demo_merge
+INTO tmp_demo_merge -- new table created
 FROM sample_data;
 
 -- INSERT if doesn't exist
 WITH data AS (
-  SELECT 1 AS group_id, 2001 AS year, 10001 AS tour_id, 'San Francisco' AS city
+  SELECT 7 AS tour_id, 4 AS group_id, 2021 AS year, 'San Francisco' AS city
 )
 INSERT
-INTO tmp_demo_merge(group_id, year, tour_id, city)
-SELECT group_id, year, tour_id, city
+INTO tmp_demo_merge(tour_id, group_id, year, city)
+SELECT tour_id, group_id, year, city
 FROM data
 WHERE NOT exists(SELECT 1
-                 FROM
-                   tmp_demo_merge tdm
-                     JOIN data ON tdm.tour_id = data.tour_id);
+                 FROM tmp_demo_merge tdm
+                   JOIN data ON tdm.tour_id = data.tour_id);
 
 -- MERGE
-WITH data AS (
-  SELECT 1 AS group_id, 2001 AS year, 10001 AS tour_id, 'San Francisco' AS city
-) MERGE INTO tmp_demo_merge tdm
+MERGE INTO tmp_demo_merge AS tdm
 USING (
-  SELECT *
-  FROM data
-) sq
-ON (tdm. = sq.)
+  SELECT 7 AS tour_id, 4 AS group_id, 2021 AS year, 'San Francisco' AS city
+) data
+ON (tdm.tour_id = data.tour_id)
 WHEN MATCHED THEN
   UPDATE
-  SET tdm. = sq., tdm. = sq.
+  SET tdm.group_id = data.group_id, tdm.year = data.year, tdm.city = data.city
 WHEN NOT MATCHED THEN
   INSERT
-    (, )
+    (tour_id, group_id, year, city)
   VALUES
-    (sq., sq.);
+    (data.tour_id, data.group_id, data.year, data.city);
+
+DROP TABLE tmp_demo_merge;
