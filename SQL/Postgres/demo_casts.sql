dc4dd9922c9d28a30a819d609206fb4b9b48c9a3
Dmitriy "DK" Korobskiy <dkroot2@gmail.com>
2021-09-15 14:44:27 -0400

    DK

--- SQL/Postgres/demo_casts.sql
+++ SQL/Postgres/demo_casts.sql
@@ -1,1 +1,7 @@
-SELECT (TRUE :: INT) AS true_int, (FALSE :: INT) AS false_int;
+SELECT cast(TRUE AS INTEGER) AS true_int, cast(FALSE AS INTEGER) AS false_int;
+
+SELECT
+  CASE WHEN 'foo' IS NOT NULL THEN TRUE ELSE FALSE END AS true_bool,
+  CASE WHEN NULL IS NOT NULL THEN TRUE ELSE FALSE END AS false_bool;
+
+SELECT ('foo' IS NOT NULL) AS true_bool, (NULL IS NOT NULL) AS false_bool;
