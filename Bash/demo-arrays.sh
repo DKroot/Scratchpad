5fa8a8fb2c4ee7891eb3d15792aba77196f29dbc
Dmitriy "DK" Korobskiy <dkroot2@gmail.com>
2021-09-16 16:26:38 -0400

    DK

--- Bash/demo-arrays.sh
+++ Bash/demo-arrays.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 declare -a arr=(foo bar baz)
+
+echo "IFS-separated array: \`${arr[*]}\`"
+
 printf -v JOINED_ARR -- '--file=%s ' "${arr[@]}"
 echo "Joined array: \`$JOINED_ARR\`"
