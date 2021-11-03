7d3f6987016d2690c74a8bf6cc017cfb5a57a971
Dmitriy "DK" Korobskiy <dkroot2@gmail.com>
2021-11-03 14:56:41 -0400

    DK

--- SQL/Postgres/demo_aggregation.sql
+++ SQL/Postgres/demo_aggregation.sql
@@ -1,48 +1,47 @@
 -- Implicit GROUP = the entire result set
-  WITH cte(a, b) AS (
-    VALUES
-      ('foo', NULL),
-      ('foo', 'baz'),
-      ('foo', 'qux'),
-      ('bar', 'baz'),
-      ('baz', NULL)
-  )
+WITH cte(a, b) AS (
+  VALUES
+    ('foo', NULL),
+    ('foo', 'baz'),
+    ('foo', 'qux'),
+    ('bar', 'baz'),
+    ('baz', NULL)
+)
 SELECT count(a)
-  FROM cte;
+FROM cte;
 
 -- GROUP BY and pick NOT NULLs
-  WITH cte(a, b) AS (
-    VALUES
-      ('foo', NULL),
-      ('foo', 'baz'),
-      ('foo', 'qux'),
-      ('bar', 'baz'),
-      ('baz', NULL)
-  )
+WITH cte(a, b) AS (
+  VALUES
+    ('foo', NULL),
+    ('foo', 'baz'),
+    ('foo', 'qux'),
+    ('bar', 'baz'),
+    ('baz', NULL)
+)
 SELECT a, max(b), min(b), string_agg(b, ', ')
-  FROM cte
- GROUP BY a;
+FROM cte
+GROUP BY a;
 
-  WITH cte(a, b) AS (
-    VALUES
-      ('foo', NULL),
-      ('foo', 'baz'),
-      ('foo', 'qux'),
-      ('bar', 'baz'),
-      ('baz', NULL)
-  )
+WITH cte(a, b) AS (
+  VALUES
+    ('foo', NULL),
+    ('foo', 'baz'),
+    ('foo', 'qux'),
+    ('bar', 'baz'),
+    ('baz', NULL)
+)
 SELECT 'True'
-  FROM cte
+FROM cte
 HAVING count(1) > 3;
 
 -- Counting duplicates
 SELECT COUNT(1)
-  FROM
-    (
-      VALUES
-        ('foo', 'bar'),
-        ('foo', 'baz'),
-        ('bar', 'baz'),
-        ('baz', 'qux')
-    ) AS t(a, b)
- WHERE a = 'foo';
+FROM (
+  VALUES
+    ('foo', 'bar'),
+    ('foo', 'baz'),
+    ('bar', 'baz'),
+    ('baz', 'qux')
+) AS t(a, b)
+WHERE a = 'foo';
--- SQL/Postgres/demo_arrays.sql
+++ SQL/Postgres/demo_arrays.sql
@@ -1,14 +1,25 @@
--- Unrolling an array in an expression (a-la a lambda function)
+-- Unrolling an array in an int expression (a-la a lambda function)
 WITH cte AS (
-  SELECT ARRAY[1, 2, 42] AS a
+  SELECT ARRAY [1, 2, 42] AS a
 )
-SELECT unnest(a) + 1 as expression
+SELECT unnest(a) + 1 AS expression
 FROM cte;
 
--- Unrolling an array in an expression (a-la a lambda function)
--- noinspection SqlSignature
+-- Unrolling an array in a string expression (a-la a lambda function)
 WITH cte AS (
-  SELECT ARRAY[' f oo ', ' b ar,', 'baz'] AS a
+  SELECT ARRAY [' f oo ', ' b ar,', 'baz'] AS a
 )
-SELECT translate(unnest(a), ' ,', '') as expression
+SELECT translate(unnest(a), ' ,', '') AS expression
+FROM cte;
+
+-- Implicit GROUP = the entire result set
+WITH cte(a, b) AS (
+  VALUES
+    ('foo', NULL),
+    ('foo', 'baz'),
+    ('foo', 'qux'),
+    ('bar', 'baz'),
+    ('baz', NULL)
+)
+SELECT array_agg(a) AS a_list, array_agg(b) AS b_list
 FROM cte;
