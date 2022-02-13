d2a400fcbc98b7a04695eb8f9358ca75956c3f2e
Dmitriy "DK" Korobskiy <dkroot2@gmail.com>
2022-02-13 09:12:28 -0500

    DK

--- SQL/SQLServer/demo_booleans.sql
+++ SQL/SQLServer/demo_booleans.sql
@@ -1,20 +1,11 @@
--- Select BIT data
-SELECT cast(foo AS BIT) AS foo_bool
+-- Numeric to BIT: true when <> 0
+SELECT
+--@formatter:off
+  cast(one AS BIT) AS "1", cast(zero AS BIT) AS "0", cast(two AS BIT) AS "2",
+  cast(true_upper AS BIT) AS ['TRUE'], cast(false_upper AS BIT) AS ['FALSE'],
+  cast(true_lower AS BIT) AS ['true'], cast(true_lower AS BIT) AS ['false']
+--@formatter:on
 FROM (
   VALUES --
-    (1)
-) AS sample_data(foo);
-
--- Select a Boolean expression
-SELECT cast(iif(foo IN (1, 2, 3), 1, 0) AS BIT) AS foo_bool, cast(iif(foo = 42, 1, 0) AS BIT) AS bar_bool
-FROM (
-  VALUES --
-    (42)
-) AS sample_data(foo);
-
--- Select a Boolean expression
-SELECT cast(iif(foo IN (1, 2, 3), 1, 0) AS BIT) AS foo_bool, cast(iif(foo = 42, 'TRUE', 'FALSE') AS BIT) AS bar_bool
-FROM (
-  VALUES --
-    (42)
-) AS sample_data(foo);
+    (1, 0, 2, 'TRUE', 'FALSE', 'true', 'false')
+) AS sample_data(one, zero, two, true_upper, false_upper, true_lower, false_lower);
