b1172eb6b0d4e2676c59acbcb3df0032d9e65807
Dima Korobskiy <dkroot2@gmail.com>
2023-05-24 11:10:35 -0400

    Misc

--- SQL/SQLServer/demo_assignments.sql
+++ SQL/SQLServer/demo_assignments.sql
@@ -1,33 +1,48 @@
 :ON ERROR EXIT
 -- JetBrains IDEs: start execution from here
 
--- Sample data
-SELECT *
-FROM (
-  VALUES --
-    (1, 1, 2001, 'San Francisco'),
-    (2, 1, 2009, 'Chicago'),
-    (3, 1, 2009, 'New Orleans'),
-    (4, 2, 2006, 'Washington'),
-    (5, 2, 2007, 'New York'),
-    (6, 3, 2008, 'Seattle')
-) AS sample_data(tour_id, group_id, year, city);
+DECLARE @city VARCHAR(MAX), @bar VARCHAR(MAX);
 
-DECLARE @foo VARCHAR(MAX), @bar VARCHAR(MAX);
-
-SET @foo = 'foo';
-PRINT concat('@foo=', @foo);
+SET @city = 'San Francisco';
+PRINT concat('@city', @city);
 
 SET @bar = 'bar';
-PRINT concat_ws('; ', concat('@foo=', @foo), concat('@bar=', @bar));
+PRINT concat_ws('; ', concat('@city=', @city), concat('@bar=', @bar));
 
 -- Can't use multiple assignment in one line
 -- SET @foo = 'foo', @bar = 'bar'
-SELECT @foo = 'baz', @bar = 'qux';
-PRINT concat_ws('; ', concat('@foo=', @foo), concat('@bar=', @bar));
+SELECT @city = 'New Orleans', @bar = 'qux';
+PRINT concat_ws('; ', concat('city=', @city), concat('@bar=', @bar));
 
-IF @foo = 'ba'
-  PRINT @foo;
+-- Sample data
+SELECT TOP 1 @city = city
+FROM (
+       VALUES --
+              ('San Francisco'),
+              ('Chicago'),
+              ('New Orleans'),
+              ('Washington'),
+              ('New York'),
+              ('Seattle')
+     ) AS cities(city)
+ORDER BY city;
+
+IF @city = 'Chicago'
+  PRINT @city;
 ELSE BEGIN
   PRINT 'ELSE';
 END;
+
+SELECT TOP 1 @city = city
+FROM (
+       VALUES --
+              ('San Francisco'),
+              ('Chicago'),
+              ('New Orleans'),
+              ('Washington'),
+              ('New York'),
+              ('Seattle')
+     ) AS cities(city)
+WHERE city = 'foo'
+ORDER BY city DESC;
+PRINT @city;
