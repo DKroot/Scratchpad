6af937ec828ecccb5471374590d4c1386845263f
Dima Korobskiy <dkroot2@gmail.com>
2023-05-25 17:57:06 -0400

    Dima

--- SQL/Postgres/demo_arrays.sql
+++ SQL/Postgres/demo_arrays.sql
@@ -1,25 +1,13 @@
 -- Unrolling an array in an int expression (a-la a lambda function)
-WITH cte AS (
-  SELECT ARRAY [1, 2, 42] AS a
-)
-SELECT unnest(a) + 1 AS expression
-FROM cte;
+SELECT unnest(ARRAY [1, 2, 42]) + 1 AS expression;
 
 -- Unrolling an array in a string expression (a-la a lambda function)
-WITH cte AS (
-  SELECT ARRAY [' f oo ', ' b ar,', 'baz'] AS a
-)
-SELECT translate(unnest(a), ' ,', '') AS expression
-FROM cte;
+SELECT translate(unnest(ARRAY [' f oo ', ' b ar,', 'baz']), ' ,', '') AS expression;
 
 -- Implicit GROUP = the entire result set
-WITH cte(a, b) AS (
-  VALUES
-    ('foo', NULL),
-    ('foo', 'baz'),
-    ('foo', 'qux'),
-    ('bar', 'baz'),
-    ('baz', NULL)
-)
 SELECT array_agg(a) AS a_list, array_agg(b) AS b_list
-FROM cte;
+FROM (VALUES ('foo', NULL),
+        ('foo', 'baz'),
+        ('foo', 'qux'),
+        ('bar', 'baz'),
+        ('baz', NULL)) AS cte(a, b);
--- SQL/Postgres/demo_operators.sql
+++ SQL/Postgres/demo_operators.sql
@@ -1,16 +1,16 @@
 SELECT 9 / 10;
 -- 0
 
-SELECT CAST(9 AS float) / 10;
+SELECT cast(9 AS FLOAT) / 10;
 -- 0.9
 
-SELECT CAST(9 AS numeric) / 10;
+SELECT cast(9 AS NUMERIC) / 10;
 -- 0.9
 
-SELECT round(CAST(9 AS numeric) / 10);
+SELECT round(cast(9 AS NUMERIC) / 10);
 -- 1
 
-SELECT round(CAST(1 AS numeric) / 3, 2);
+SELECT round(cast(1 AS NUMERIC) / 3, 2);
 -- 1
 
 SELECT (1, 2) = (1, 2);
@@ -22,44 +22,54 @@ SELECT (1, 2) = (1, 3);
 SELECT (1, NULL) = (1, NULL);
 -- NULL
 
-SELECT ROW (1, NULL) = ROW (1, NULL);
+-- Including an optional ROW keyword in the row constructor that builds a row value (also called a composite value)
+--@formatter:off
+SELECT ROW(1, NULL) = ROW(1, NULL);
 -- NULL
+--@formatter:on
 
-SELECT (1, NULL) IS NOT DISTINCT FROM(1, NULL);
+SELECT (1, NULL) IS NOT DISTINCT FROM (1, NULL);
 -- true
 
+-- Including an optional ROW keyword in the row constructor that builds a row value (also called a composite value)
 SELECT ROW (1, NULL) IS NOT DISTINCT FROM ROW (1, NULL);
 -- true
 
 -- Operation with a subquery value in SELECT
 WITH cte AS (--
   SELECT 1, 'a', 'aa' --
-    UNION ALL --
-    SELECT 2, 'b', NULL --
-    UNION ALL --
-    SELECT 3, 'b', NULL)
-SELECT
-    1 +
-    (SELECT min(id)
-     FROM cte t(id, foo, bar)
-     WHERE foo = 'b') AS total;
+  UNION ALL
+  --
+  SELECT 2, 'b', NULL --
+  UNION ALL
+  --
+  SELECT 3, 'b', NULL)
+SELECT 1 +
+       (SELECT min(id)
+        FROM cte t(id, foo, bar)
+        WHERE foo = 'b') AS total;
 
 -- Operation with a subquery value in WHERE
 WITH employee AS ( --
   SELECT 1, 100000 --
-    UNION ALL --
-    SELECT 2, 80000 --
-    UNION ALL --
-    SELECT 3, 100000  --
-  )
+  UNION ALL
+  --
+  SELECT 2, 80000 --
+  UNION ALL
+  --
+  SELECT 3, 100000 --
+)
 SELECT *
 FROM employee t(id, salary)
 WHERE id =
-        (WITH cte AS (--
-          SELECT 1, 'a', 'aa' --
-          UNION ALL --
-          SELECT 2, 'b', NULL --
-          UNION ALL --
-          SELECT 3, 'b', NULL) SELECT min(id)
-                               FROM cte t(id, foo, bar)
-                               WHERE foo = 'b');
+      (WITH cte AS (--
+        SELECT 1, 'a', 'aa' --
+        UNION ALL
+        --
+        SELECT 2, 'b', NULL --
+        UNION ALL
+        --
+        SELECT 3, 'b', NULL)
+       SELECT min(id)
+       FROM cte t(id, foo, bar)
+       WHERE foo = 'b');
