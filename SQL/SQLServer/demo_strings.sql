d2e2663bf5d53165a658bd553c37264f536623ab
Dima Korobskiy <dkroot2@gmail.com>
2023-06-02 12:17:40 -0400

    Misc

--- SQL/SQLServer/demo_strings.sql
+++ SQL/SQLServer/demo_strings.sql
@@ -1,45 +1,51 @@
 --region Collations
--- DB collation
-PRINT databasepropertyex(db_name(), 'collation');
--- `SQL_Latin1_General_CP1_CI_AS`
+DECLARE @foo VARCHAR = 'qw', @db_coll NVARCHAR(128) = cast(databasepropertyex(db_name(), 'collation') AS NVARCHAR);
 
-DECLARE @foo VARCHAR = 'FOO', @pay_plan CHAR(2), --
-  @db_collation NVARCHAR(128) = cast(databasepropertyex(db_name(), 'collation') AS NVARCHAR(128));
-SELECT 'Case-' + iif(@foo = 'foo', 'IN', '') + 'sensitive literal comparison with ' + @db_collation AS resul1;
+-- Literal comparison with DB default collation
+PRINT '1. Case-' + iif('QW' = 'qw', 'IN', '') + 'sensitive literal comparison';
+
+-- TODO Why the case-sensitive comparison? See https://stackoverflow.com/q/76391818/534217.
+PRINT '2. Case-' + iif(@foo = 'qw', 'IN', '') + 'sensitive literal comparison with the DB default ' + @db_coll;
+
+-- TODO Why the case-sensitive comparison? See https://stackoverflow.com/q/76391818/534217.
+PRINT '3. Case-' +
+      iif(@foo COLLATE sql_latin1_general_cp1_ci_as = 'qw' COLLATE sql_latin1_general_cp1_ci_as, 'IN', '') +
+      'sensitive literal comparison with the specified SQL_Latin1_General_CP1_CI_AS';
+GO
+
+DECLARE @pay_plan CHAR(2);
+
 SELECT @pay_plan = pay_plan_code
 FROM dev.indv_info
 WHERE indv_ssn_id = '100058314';
--- 'EI'
-SELECT 'Case-' + iif(@pay_plan = 'ei', 'IN', '') + 'sensitive data comparison with ' + @db_collation AS resul2;
-SELECT
-  'Case-' + iif(@pay_plan COLLATE sql_latin1_general_cp1_ci_as = 'ei', 'IN', '')
-      + 'sensitive data comparison with SQL_Latin1_General_CP1_CI_AS' AS resul3;
-SELECT
-  'Case-' + iif(@pay_plan COLLATE sql_latin1_general_cp1_cs_as = 'ei', 'IN', '')
-      + 'sensitive data comparison with SQL_Latin1_General_CP1_CS_AS' AS resul4;
-GO
 
--- LIKE pattern ranges are always case-insensitive
-SELECT s AS acronym
-FROM (
-  VALUES --
-    ('FOO'),
-    ('foo'),
-    ('BAR'),
-    ('bar')
-) AS t(s)
-WHERE s COLLATE sql_latin1_general_cp1_cs_as LIKE '[A-Z][A-Z][A-Z]' COLLATE sql_latin1_general_cp1_cs_as;
+-- 'EI'
+PRINT 'Case-' + iif(@pay_plan = 'ei', 'IN', '') + 'sensitive data comparison ';
+PRINT 'Case-' +
+      iif(@pay_plan COLLATE sql_latin1_general_cp1_ci_as = 'ei' COLLATE sql_latin1_general_cp1_ci_as, 'IN', '') +
+      'sensitive data comparison with SQL_Latin1_General_CP1_CI_AS';
+PRINT 'Case-' +
+      iif(@pay_plan COLLATE sql_latin1_general_cp1_cs_as = 'ei' COLLATE sql_latin1_general_cp1_cs_as, 'IN', '') +
+      'sensitive data comparison with SQL_Latin1_General_CP1_CS_AS';
+GO
 
 -- Case-sensitive LIKE pattern
 SELECT s AS acronym
-FROM (
-  VALUES --
-    ('FOO'),
-    ('foo'),
-    ('BAR'),
-    ('bar')
-) AS t(s)
-WHERE s COLLATE sql_latin1_general_cp1_cs_as LIKE '%[ABCDEFGHIJKLMNOPQRSTUVWXYZ]%';
+FROM (VALUES --
+        ('FOO'),
+        ('foo'),
+        ('BAR'),
+        ('bar')) AS t(s)
+WHERE s LIKE '%[ABCDEFGHIJKLMNOPQRSTUVWXYZ]%' COLLATE sql_latin1_general_cp1_cs_as;
+
+-- LIKE pattern ranges are always case-insensitive
+SELECT s AS acronym
+FROM (VALUES --
+        ('FOO'),
+        ('foo'),
+        ('BAR'),
+        ('bar')) AS t(s)
+WHERE s LIKE '[A-Z][A-Z][A-Z]' COLLATE sql_latin1_general_cp1_cs_as;
 --endregion
 
 PRINT coalesce(NULL, '.');
@@ -60,23 +66,15 @@ FROM string_split('BHW Management Information System Solution', ' ');
 SELECT string_agg(left(value, 1), '')
 FROM string_split(translate('OPAIS/340B Pricing System', '/', ' '), ' ');
 
-SELECT
-  iif(cte.name LIKE '%(%)%',
-      substring(cte.name, charindex('(', cte.name) + 1, charindex(')', cte.name) - charindex('(', cte.name) - 1), (
-        SELECT string_agg(left(value, 1), '')
-        FROM string_split(cte.name, ' ')
-      )) AS code
-FROM (
-  VALUES --
-    ('Electronic Handbooks (EHBs)')
-) AS cte(name);
+SELECT iif(cte.name LIKE '%(%)%',
+           substring(cte.name, charindex('(', cte.name) + 1, charindex(')', cte.name) - charindex('(', cte.name) - 1),
+           (SELECT string_agg(left(value, 1), '')
+            FROM string_split(cte.name, ' '))) AS code
+FROM (VALUES --
+        ('Electronic Handbooks (EHBs)')) AS cte(name);
 
-SELECT (
-  SELECT string_agg(left(value, 1), '')
-  FROM string_split(cte.name, ' ')
-) AS code
-FROM (
-  VALUES --
-    ('BHW Management Information System Solution')
-) AS cte(name);
+SELECT (SELECT string_agg(left(value, 1), '')
+        FROM string_split(cte.name, ' ')) AS code
+FROM (VALUES --
+        ('BHW Management Information System Solution')) AS cte(name);
 --endregion
