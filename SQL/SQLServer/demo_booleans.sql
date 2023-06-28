3bbd34c58c771081520ea65bc05068876a665f13
Dima Korobskiy <dkroot2@gmail.com>
2023-06-28 09:12:30 -0400

    Misc

--- SQL/SQLServer/demo_booleans.sql
+++ SQL/SQLServer/demo_booleans.sql
@@ -1,21 +1,20 @@
 -- Numeric to BIT: true when <> 0
-SELECT
---@formatter:off
-  cast(one AS BIT) AS "1", cast(zero AS BIT) AS "0", cast(two AS BIT) AS "2",
-  cast(true_upper AS BIT) AS ['TRUE'], cast(false_upper AS BIT) AS ['FALSE'],
-  cast(true_lower AS BIT) AS ['true'], cast(true_lower AS BIT) AS ['false']
---@formatter:on
-FROM (
-  VALUES --
-    (1, 0, 2, 'TRUE', 'FALSE', 'true', 'false')
-) AS sample_data(one, zero, two, true_upper, false_upper, true_lower, false_lower);
+SELECT cast(one AS BIT) AS "1", cast(zero AS BIT) AS "0", cast(two AS BIT) AS "2", cast(true_upper AS BIT) AS ['TRUE'],
+  cast(false_upper AS BIT) AS ['FALSE'], cast(true_lower AS BIT) AS ['true'], cast(true_lower AS BIT) AS ['false']
+FROM (VALUES --
+        (1, 0, 2, 'TRUE', 'FALSE', 'true',
+         'false')) AS sample_data(one, zero, two, true_upper, false_upper, true_lower, false_lower)
 
 IF (SELECT 'foo') = 'foo'
-  PRINT 'true';
+  BEGIN
+    PRINT 'SELECT ''foo'' = ''foo'''
+    PRINT 'true'
+  END
 ELSE
-  PRINT 'false';
+  PRINT 'false'
 
-IF (SELECT 'foo' WHERE 1 = 0) IS NULL
-  PRINT 'true';
+IF (SELECT 'foo'
+    WHERE 1 = 0) IS NULL
+  PRINT 'true'
 ELSE
-  PRINT 'false';
+  PRINT 'false'
