a385c81917f59469a8f883a94204861955156afc
Dima Korobskiy <dkroot2@gmail.com>
2023-08-31 08:40:38 -0400

    DK

---
+++ Bash/demo-cmd-groups.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+var1=$(echo foo; echo bar)
+echo -e "\nvar1=\`$var1\`"
+
+echo
+time { echo foo; echo bar; }
+
+# Bash 5.1.16: success
+# Bash 5.2.15: syntax error near unexpected token `}'
+var2=$(time { echo foo; echo bar; })
+echo -e "\nvar2=\`$var2\`"
+
+time (echo foo; echo bar)
+
+# Bash 5.1.16: success
+# Bash 5.2.15: syntax error near unexpected token `)'
+var3=$(time (echo foo; echo bar))
+echo -e "\nvar3=\`$var3\`"
+
+time var4=$(echo foo; echo bar)
+echo -e "\nvar4=\`$var4\`"
