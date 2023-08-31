0e4fa01d4917cbc719d305c28b601e914324d721
Dima Korobskiy <dkroot2@gmail.com>
2023-08-31 11:53:24 -0400

    DK

--- Bash/demo-arrays.sh
+++ Bash/demo-arrays.sh
@@ -2,18 +2,22 @@
 set -e
 set -o pipefail
 
-declare -a arr=(foo 'bar baz' qux)
+a=(foo 'bar baz' qux)
 
-IFS=';'
-echo "IFS-separated array interpolation: \`${arr[*]}\`"
+IFS=';' # IFS can't be passed on the fly to echo
+echo "IFS-separated array interpolation ([*]): \`${a[*]}\`"
 unset IFS
 
-bash -c 'echo Passing arg array: 1=\`$1\`, 2=\`$2\`, 3=\`$3\`, 4=\`$4\`, 5=\`$5\`. Number of args=$#.' - "${arr[@]}"
+IFS=';'
+echo "Expanded space-separated array ([@]). IFS has no effect:" "${a[@]}"
+bash -c 'echo Passing args: 1=\`$1\`, 2=\`$2\`, 3=\`$3\`, 4=\`$4\`. Number of args=$#.' - "${a[@]}"
+time (echo 'Expanded space-separated array in a subshell:'; echo "${a[@]}")
+unset IFS
 
-printf -v JOINED_ARR '%s;' "${arr[@]}"
+printf -v JOINED_ARR '%s;' "${a[@]}"
 echo "Joined array: \`$JOINED_ARR\`"
 
-printf -v JOINED_ARR -- "--file='%s' " "${arr[@]}"
+printf -v JOINED_ARR -- "--file='%s' " "${a[@]}"
 echo "Join + format each segment: \`$JOINED_ARR\`"
 
 head_and_tail() {
@@ -38,4 +42,4 @@ head_and_tail() {
   done
 }
 
-head_and_tail "${arr[@]}"
+head_and_tail "${a[@]}"
--- Bash/demo-cmd-groups.sh
+++ Bash/demo-cmd-groups.sh
@@ -6,19 +6,20 @@ var1=$(echo foo; echo bar)
 echo -e "\nvar1=\`$var1\`"
 
 echo
-time { echo foo; echo bar; }
+time { echo foo; printf "It took:" >&2; }
 
 # Bash 5.1.16: success
 # Bash 5.2.15: syntax error near unexpected token `}'
-var2=$(time { echo foo; echo bar; })
-echo -e "\nvar2=\`$var2\`"
+#var2=$(time { echo foo; echo bar; })
+#echo -e "\nvar2=\`$var2\`"
 
-time (echo foo; echo bar)
+echo
+time (echo foo; printf "It took:" >&2)
 
 # Bash 5.1.16: success
 # Bash 5.2.15: syntax error near unexpected token `)'
-var3=$(time (echo foo; echo bar))
-echo -e "\nvar3=\`$var3\`"
+#var3=$(time (echo foo; echo bar))
+#echo -e "\nvar3=\`$var3\`"
 
 time var4=$(echo foo; echo bar)
 echo -e "\nvar4=\`$var4\`"
