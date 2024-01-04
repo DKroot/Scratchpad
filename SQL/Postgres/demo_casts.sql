8a5851673b1eb28626f5c4eb1337bdb046b77492
Dima Korobskiy <dkroot2@gmail.com>
2024-01-04 16:34:16 -0500

    Dima

--- SQL/Postgres/demo_casts.sql
+++ SQL/Postgres/demo_casts.sql
@@ -1,10 +1,11 @@
 SELECT cast(TRUE AS INTEGER) AS true_int, cast(FALSE AS INTEGER) AS false_int;
 
-SELECT
-  CASE WHEN 'foo' IS NOT NULL THEN TRUE ELSE FALSE END AS true_bool,
+SELECT CASE WHEN 'foo' IS NOT NULL THEN TRUE ELSE FALSE END AS true_bool,
   CASE WHEN NULL IS NOT NULL THEN TRUE ELSE FALSE END AS false_bool;
 
 SELECT ('foo' IS NOT NULL) AS true_bool, (NULL IS NOT NULL) AS false_bool;
+
+SELECT cast('42' AS INTEGER) AS number, cast(NULL AS INTEGER) AS null_int, cast(nullif('', '') AS INTEGER) AS blank_int;
 
 
 
