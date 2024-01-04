18c3df42cd0957eb4dde795a1754cf08fbcd0ac0
Dima Korobskiy <dkroot2@gmail.com>
2024-01-04 16:42:15 -0500

    Dima

--- SQL/Postgres/demo_casts.sql
+++ SQL/Postgres/demo_casts.sql
@@ -7,6 +7,17 @@ SELECT ('foo' IS NOT NULL) AS true_bool, (NULL IS NOT NULL) AS false_bool;
 
 SELECT cast('42' AS INTEGER) AS number, cast(NULL AS INTEGER) AS null_int, cast(nullif('', '') AS INTEGER) AS blank_int;
 
+CREATE TEMPORARY TABLE IF NOT EXISTS tmp_demo_casts (
+  foo INTEGER
+);
+
+INSERT INTO tmp_demo_casts AS tdc(foo)
+VALUES ('42');
+
+INSERT INTO tmp_demo_casts AS tdc(foo)
+VALUES (cast(nullif('', '') AS INTEGER));
+
+SELECT * FROM tmp_demo_casts;
 
 
 
