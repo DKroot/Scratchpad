c48ae92aa2d007f1e0741c4bae92f8db02f0fa3f
Dima Korobskiy <dkroot2@gmail.com>
2024-09-25 21:18:08 -0400

    Misc

--- SQL/SQLServer/demo_dates.sql
+++ SQL/SQLServer/demo_dates.sql
@@ -37,12 +37,18 @@ SELECT cast(1 AS BIT) AS plus_minus_day
 WHERE getdate() BETWEEN getdate() - 1 AND coalesce(NULL, getdate() + 1);
 
 -- Is the date after '2024-01-01'
-SELECT CAST(iif(getdate() >= '2024-01-01', 1, 0) AS BIT) AS date_after_2024;
+SELECT cast(iif(getdate() >= '2024-01-01', 1, 0) AS BIT) AS date_after_2024;
+GO
 
 -- Literals and difference
-DECLARE @foo DATE = '2021-08-01', @bar DATETIME = '2021-08-02T21:00:42', @baz DATETIME = '2021-08-02T21:15:42';
-PRINT concat_ws(', ', @foo, @bar, @baz);
+DECLARE @foo DATE = '2021-08-09', @bar DATETIME = '2021-08-10T21:00:42';
+PRINT @foo
+PRINT @bar
+PRINT concat_ws(', ', @foo, @bar);
 PRINT concat_ws(' ', 'DATETIME - DATE =', datediff(DAY, @foo, @bar), 'day');
+PRINT concat('The number of months since day 0 = ', datediff(MONTH, 0, @foo));
+PRINT concat('The first day of the month = ', dateadd(MONTH, datediff(MONTH, 0, @foo), 0));
+PRINT concat('The last day of the month = ', eomonth(@foo));
 GO
 
 -- Format seconds as hh:mm:ss
@@ -51,5 +57,5 @@ PRINT '3600 seconds = ' + convert(CHAR(8), dateadd(SECOND, 3600, 0), 108) + ' hh
 PRINT '23 hours + 1 second = ' + convert(CHAR(8), dateadd(SECOND, 23 * 3600 + 1, 0), 108) + ' hh:mm:ss';
 
 -- WARNING Overflows on 24 hours!
-PRINT 'WARNING Overflows on 24 hours! 24 hours = ' + convert(VARCHAR(10), dateadd(SECOND, 24 * 3600, 0), 108)
-    + ' hh:mm:ss';
+PRINT concat('WARNING Overflows on 24 hours! 24 hours = ', --
+             dateadd(SECOND, 24 * 3600, 0), ' hh:mm:ss');
